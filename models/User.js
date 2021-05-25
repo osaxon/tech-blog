de490ba4de0d142a436eb02ca4f4cdb026ba0744
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-25 16:08:36 +0100

    User model created with beforeCreate hook

--- models/User.js
+++ models/User.js
@@ -1,10 +1,54 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+const { beforeCreate } = require('./Post');
 
 class User extends Model {}
 
 User.init(
     // define columes
+    // id PK
+    // username
+    // email
+    // password
+
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            },
+        }
+
+    },
+
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.email = await newUserData.email.toLowerCase();
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUser
+            },
+        }
+    },
 
     {
         sequelize,
