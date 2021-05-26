3420cac7d8cdd8530478095420148c9c97ddae9d
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-26 21:23:12 +0100

    Setting up session and cookies

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 
 const app = express();
 const PORT = process.env.port || 3300;
 
-const hbs = exphbs.create({});
+const sess = {
+    secret: 'super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        expiration: 24 * 60 * 60 * 1000, // 24hrs
+    })
+}
 
+const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
