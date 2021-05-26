468a0734c8fbc3a557e073e810dc9ee2ac70e336
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-26 13:05:12 +0100

    Get user by id api route working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,10 +3,24 @@ const { User, Post, Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
     try{
-        const userData = await User.findAll({
-            include: [{ model: Post}],
-        });
+        const userData = await User.findAll();
         res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    console.log("Searching for user ID: ", req.params.id)
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password']}
+        })
+        res.status(200).json(userData)
+        if(!userdata){
+            res.status(404).json("User not found with this ID")
+        }
+        
     } catch (err) {
         res.status(500).json(err);
     }
