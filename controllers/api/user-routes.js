8006f001360b47ad29f972c5e78f10f1692206c8
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-28 13:14:34 +0100

    Login API route created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,6 +10,23 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email }});
+
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password, please try again."})
+            return;
+        }
+
+        const validPassword = await userData.checkPassword()
+
+        
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 router.get('/:id', async (req, res) => {
     try {
         const userData = await User.findByPk(req.params.id, {
