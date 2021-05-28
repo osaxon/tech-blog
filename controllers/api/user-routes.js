bc76d574821932a5ce0888f558f459b72a5ba0a6
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-28 13:25:07 +0100

    Login script and route created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,9 +19,21 @@ router.post('/login', async (req, res) => {
             return;
         }
 
-        const validPassword = await userData.checkPassword()
+        const validPassword = await userData.checkPassword(req.params.password);
 
-        
+        if(!validPassword) {
+            res.status(400).json({ message: "Incorrect email or password, please try again."})
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: "You are now logged in"})
+        })
+
+
     } catch (err) {
         res.status(400).json(err)
     }
