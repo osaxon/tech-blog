8cb3792d06862b00523b6310659a56e4e005f24c
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-28 11:54:47 +0100

    Login page and route created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try{
-        const postData = await Post.findAll();
+        const postData = await Post.findAll({
+            attributes: { exclude: ['password']},
+        });
         // serialise the raw data
         const posts = postData.map((post) => post.get({ plain: true}))
         res.render('homepage', {posts})
     } catch (err) {
         res.status(500).json(err);
     }
+})
+
+router.get('/login', (req, res) => {
+    res.render('login')
 })
 
 module.exports = router;
