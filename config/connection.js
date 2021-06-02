ec850b72e8722cf16bead317abacc3cff078b509
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-02 15:51:04 +0100

    Added JAWS DB env

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,25 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    console.log("JAWSDB")
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    console.log("LOCAL DB")
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+
 
 module.exports = sequelize;
